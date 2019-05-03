152232d8a53ddf7c43ff078e14b75e0ed454b031
emitdlr <emit@utexas.edu>
2019-05-03 09:24:11 -0500

    css additions and panels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MaterialModule } from './material';
 import { AppComponent } from "./app.component";
 import { WeatherComponent } from "./weather/weather.component";
 import { RouterModule } from "@angular/router";
 import { allAppRoutes } from "./routes";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, WeatherComponent],
+  declarations: [AppComponent, WeatherComponent, NavbarComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(allAppRoutes),
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
